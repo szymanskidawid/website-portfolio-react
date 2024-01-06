36e4d78c9de6d96e9512ed0496dd9800c83fd2e6
Dawid Szymanski <dawidszymanski.dev@outlook.com>
2024-01-06 21:58:39 +0100

    new contexts created for functionalities

--- src/components/contexts/ContextProviders.js
+++ src/components/contexts/ContextProviders.js
@@ -1,8 +1,14 @@
 import { LightModeContext } from './LightModeContext';
+import { LanguageContext } from './LanguageContext';
+import { ProjectContext } from './ProjectContext';
 
-const ContextProviders = ({ children, lightMode, setLightMode }) => (
+const ContextProviders = ({ children, lightMode, setLightMode, selectedProject, setSelectedProject, language, setLanguage }) => (
   <LightModeContext.Provider value={{ lightMode, setLightMode }}>
-    {children}
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      <ProjectContext.Provider value={{ selectedProject, setSelectedProject }}>
+        {children}
+      </ProjectContext.Provider>
+    </LanguageContext.Provider>
   </LightModeContext.Provider>
 );
 
