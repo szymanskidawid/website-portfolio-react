b0e725fe83f9e7750ec29ba913cbcbe9320b109d
Dawid Szymanski <dawidszymanski.dev@outlook.com>
2024-02-04 20:35:42 +0100

    file renamed, imports added

--- src/e-commerce-store/src/AppECommerce.js
+++ src/e-commerce-store/src/AppECommerce.js
@@ -1,7 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import ContextProviders from './contexts/ContextProviders';
 import { fetchAllProducts } from './helpers/fetchAllProducts';
-import Router from './helpers/Router';
+import { Router } from './helpers/Router';
+
+import './styles/header-section/Header.css';
+import './styles/header-section/Logo.css';
+import './styles/buttons/CheckoutButton.css';
+import './styles/buttons/RemoveButton.css';
+import './styles/main-section/CategoriesDropDown.css';
+import './styles/main-section/ProductGrid.css';
+import './styles/main-section/ProductCard.css';
+import './styles/basket-section/BasketGrid.css';
+import './styles/basket-section/BasketProduct.css';
+import './styles/buttons/MainButton.css';
+import './styles/Footer.css';
+import './styles/Variables.css';
+import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function AppECommerce() {
   const [darkMode, setDarkMode] = useState(false);
